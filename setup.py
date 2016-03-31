efa82f91b75353e3736902009f8b9eae4fb6f61e
Baran Bartu Demirci <baranbartu@Barans-MacBook-Air.local>
2016-03-31 14:00:37 +0300

    updated readme

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
+
+version = '0.0.1'
 
 setup(
     name='redis-clu',
-    version='0.0.1',
+    version=version,
     description='Redis Cluster Management Tool',
     long_description=open('README.md').read(),
     url='https://github.com/baranbartu/redis-clu',
-    author='baranbartu',
+    download_url='https://github.com/baranbartu/redis-clu/tarball/%s' % version,
+    author='Baran Bartu Demirci',
     author_email='bbartu.demirci@gmail.com',
     license='MIT',
-    keyswords='redis cluster management replication master slave',
-    packages=find_packages(),
+    keywords='redis cluster management replication master slave',
+    packages=['redisclu'],
     entry_points={
         'console_scripts': ['redis-clu = redisclu.cli:main']
     },
